afcc2927f7b6d3bea585925dc85cec4b9be086bd
Andy Lievertz <andy.lievertz@gmail.com>
2023-03-12 15:38:13 -0600

    reworked

--- README.md
+++ README.md
@@ -2,68 +2,82 @@
 
 This script automates the use of rdiff-backup to perform incremental backups of remote linux systems over SSH. It will mount a dedicated volume, generate an increment, perform increment maintenance, and unmount the volume.
 
-Place this script in `/usr/local/sbin`. It expects a single command-line argument specifying the path to a job configuration file.
+It runs with no arguments. When executed, it iterates through the *CONF* files in `LCONF_DIR` (`/etc/rdiff-backup.conf.d`) and performs the configured rdiff-backup job.
 
 Please visit https://onezeroone.dev/automating-rdiff-backup-with-bash for more information.
 
-## Prerequisite Local Configuration
+## Setup and Configuration
 
-- Generate your `root` user SSH keys with `# ssh keygen`
-- Install your `root` user SSH keys to the remote host with e.g., `ssh-copy-id -i root@rdiff-host.example.com`
-- Create a directory for configuration files e.g., `/etc/rdiff-backup.conf.d`
-- Create a configuration file for the remote host containing:
+Choose an "Rdiff-Backup System" for running the script and storing the incremental backups. The hosts that are backed up are the "Remote System(s)"
+
+### Designate a Partition for Rdiff-Backup Operations
+
+This script requires a dedicated volume that is mounted before running rdiff-backup jobs and unmounted when they are complete. On the Rdiff-Backup System (as `root`):
+
+- Create a partition (this could be on an external device) for storing rdiff increments and format it with the filesystem of your choice. This example uses *XFS*.
+- Obtain the UUID of your filesystem with the `blkid` command.
+- Create a mountpoint e.g., `/srv/rdiff`.
+- Test mounting the partition by UUID with with e.g., where "xxx..." is your `UUID`:
+
+    `# mount UUID="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" /srv/rdiff`
+
+- Unmount the partition with `# umount /srv/rdiff`
+- Add a *noauto* entry in `/etc/fstab` for your UUID, e.g.:
+
+    `UUID=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx /srv/rdiff xfs  noauto   0 0`
+
+### Clone the Repository and Copy Files
+
+Clone this repository to a temporary directory on the Rdiff-Backup System. Then (as `root`):
+
+- Copy `rdiff-backup.sh` to `/etc/cron.daily` and set permissions to `rwx------` (`0700`)
+- Copy `rdiff-backup.conf` to `/etc`
+- Modify `/etc/rdiff-backup.conf` to suit your environment:
+
+  |Variable|Example Value|Description|
+  |--------|-------------|-----------|
+  |LCONF_DIR|`"/etc/rdiff-backup.conf.d"`|Directory where the scipt will find rdiff-backup job CONF files|
+  |LUUID|`"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"`|The UUID of the partition designated for rdiff-backup operations|
+  |LMOUNTPOINT|`"/srv/rdiff"`|The mountpoint for the rdiff-backup volume|
+  |LBACKUP_DIRNAME|`"backup"`|Name of the subdirectory of LMOUNTPOINT for storing backup increments|
+  LRESTORE_DIRNAME|`"restore"`|Name of the subdirectory of LMOUNTPOINT that can be used for restore operations|
+  |RDEF_INCLUDES|`"/etc,/home,/root,/usr/local,/var"`|Directories to include for every job|
+  |RDEF_EXCLUDES|`"/,/var/lib/mysql"`|Directories to exclude from every job|
+  |LFSCK_DAY|`1`|Day to run `fsck` on the `UUID`; Sunday=0 through Saturday=6|
+
+- Create `/etc/rdiff-backup.conf.d`
+- Use `rdiff-host.example.com.conf` as a template to create a *CONF* file in `/etc/rdiff-backup.conf.d` for each Remote System.
 
     |Variable|Example Value|Description|
-    |--------|-------------|-----------| 
-    |HOST|`"rdiff-host.example.com"`|Fully qualified domain name for the remote linux host|
-    |AGE|`180`|How long to keep increments (days)|
-    |SSHPORT|`22`|SSH port for establishing a connection to the remote host|
-    |HOST_INCLUDES|`"/srv/plex,/usr/lib/plexmediaserver"`|Comma-separated list of *additional* inclusions (see *default inclusions*, below)|
-    |HOST_EXCLUDES|`"/var/lib/pgsql"`|Comma-separated list of *additional* exclusions (see *default exclusions*, below)|
+    |--------|-------------|-----------|
+    |RHOSTUSER|`root`|User that performs rdiff-backup on the Remote System|
+    |RHOSTFQDN|`"rdiff-host.example.com"`|Fully qualified domain name of the Remote System|
+    |RSSHPORT|`22`|SSH port for establishing a connection to the remote host|
+    |RHOST_INCLUDES|`"/srv/plex,/usr/lib/plexmediaserver"`|Comma-separated list of *additional* inclusions for this remote system.|
+    |RHOST_EXCLUDES|`"/var/lib/pgsql"`|Comma-separated list of *additional* exclusions for this remote system.|
+    |RAGE|`180`|How many increments to keep (days)|
 
-    E.g., `/etc/rdiff-backup.conf.d/rdiff-host.example.com.conf`:
+###  SSH Setup
 
-    ```sh
-    HOST="rdiff-host.example.com"
-    AGE="180"
-    SSHPORT="22"
-    HOST_INCLUDES=""
-    HOST_EXCLUDES=""
-    ```
+#### Rdiff-Backup System
 
-- Create a partition (this could be on an external device) for storing rdiff increments, format it, and create a mountpoint e.g., `/srv/rdiff`. Obtain the UUID for this volume with `blkid`.
+On the Rdiff-Backup System (as `root`):
 
-- Update `/usr/local/sbin/rdiff-backup.sh` with the `MOUNTPOINT` and `UUID` you generated, above.
+- Generate SSH keys for the `root` user:
 
-- Add an entry in `/etc/fstab` e.g., for an `xfs`-formatted volume:
+    `# ssh keygen`
 
-    `UUID=9e865845-f9a6-4995-aab5-d9ae952f7be0 /srv/rdiff xfs  noauto   0 0`
+- Install your `root` user SSH keys to each of the Remote System(s) with e.g.:
 
-- Create a cron job that executes an rdiff-backup increment on your desired interval e.g., `/etc/cron.d/rdiff-backup` containing:
+    `ssh-copy-id -i root@rdiff-host.example.com`
 
-    `00 04  *  *  * root /usr/local/sbin/rdiff-backup.sh /etc/rdiff-backup.conf.d/rdiff-host.example.com.conf`
+#### Remote System(s)
 
-## Prerequisite Remote Configuration
+On the Remote System(s) (as `root`), install `rdiff-backup`.
 
-- Install `rdiff-backup`:
-  - RedHat: `dnf -y install rdiff-backup`
-  - Debian: `apt-get install rdiff-backup`
-- Edit `/root/.ssh/authorized_keys` and prepend the shared key with:
-    `command="/usr/bin/rdiff-backup --server --restrict-read-only /"`
+- RedHat: `dnf install rdiff-backup`
+- Debian: `apt-get install rdiff-backup`
 
-## Default Inclusions
+Edit `/root/.ssh/authorized_keys` and prepend the shared key with:
 
-```sh
-/etc
-/home
-/usr/local
-/var
-```
-
-### Default Exclusions
-
-```sh
-/
-/var/lib/mysql
-${MOUNTPOINT}
-```
+`command="/usr/bin/rdiff-backup --server --restrict-read-only /"`
--- rdiff-backup.sh
+++ rdiff-backup.sh
@@ -1,122 +1,330 @@
 #!/bin/bash
 
-# User-definable variables
-# UUID of the rdiff-backup volume
-UUID="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-# Mount point for the rdiff-backup volume
-MOUNTPOINT="/srv/rdiff"
-# Location of configuration files
-CONF_DIR="/etc/rdiff-backup.conf.d"
-# Default directories to include for all hosts
-DEF_INCLUDES="/etc,/home,/root,/usr/local,/var"
-# Default directories to exclude for all hosts
-DEF_EXCLUDES="/,/var/lib/mysql"
-# Day to run fsck, Sunday=0 through Saturday=6
-FSCK_DAY=1
+# FUNCTIONS
+function ozo-log {
+  ### Logs output to the system log
+  if [[ -z "${LEVEL}" ]]
+  then
+    LEVEL="info"
+  fi
+  if [[ -n "${RHOSTFQDN}" ]]
+  then
+    MESSAGE="${RHOSTFQDN}: ${MESSAGE}"
+  fi
+  if [[ -n "${MESSAGE}" ]]
+  then
+    logger -p local0.${LEVEL} -t "OZO Rdiff-Backup" "${MESSAGE}"
+  fi
+}
 
-# === MAIN ====================================================================
-# === DO NOT MODIFY ANYTHING BEYOND THIS POINT ================================
-# =============================================================================
+function ozo-mount-uuid {
+  ### Mounts the LUUID to the LMOUNTPOINT
+  ### Returns 0 (TRUE) if the mount is successful and 1 (FALSE) if the mount fails
+  local RETURN=0
+  # check that the mount command is available
+  if which mount
+  then
+    # check if our device is present in the system
+    if ls /dev/disk/by-uuid/${LUUID} > /dev/null
+    then
+      # device is present; check if it is already mounted to LMOUNTPOINT
+      if mount | grep "$(readlink -f /dev/disk/by-uuid/${LUUID}) on ${LMOUNTPOINT}" > /dev/null
+      then
+        # device is present and mounted to LMOUNTPOINT; log
+        LEVEL="err" MESSAGE="Device ${LUUID} is already mounted to ${LMOUNTPOINT}." ozo-log
+        RETURN=1
+      elif mount | grep "$(readlink -f /dev/disk/by-uuid/${LUUID})"
+        # device is mounted but not to LMOUNTPOINT; log
+        LEVEL="err" MESSAGE="Found ${LUUID} mounted to a directory other than ${LMOUNTPOINT}." ozo-log
+        RETURN=1
+      else
+        # our device is present but not mounted; check if mountpoint exists
+        if [[ -d "${LMOUNTPOINT}" ]]
+        then
+          # mountpoint exists; attempt to mount
+          if mount ${LUUID} ${LMOUNTPOINT}
+          then
+            # mount succeeded; attempt to create backup and restore directories
+            if mkdir -p "${LMOUNTPOINT}/${LBACKUP_DIRNAME}","${LMOUNTPOINT}/${LRESTORE_DIRNAME}"
+            then
+              # created backup and restore directories
+              LEVEL="info" MESSAGE="Created ${LMOUNTPOINT}/${LBACKUP_DIRNAME} and ${LMOUNTPOINT}/${LRESTORE_DIRNAME}." ozo-log
+            else
+              # failed to create backup and restore directories
+              LEVEL="err" MESSAGE="Unable to create ${LMOUNTPOINT}/${LBACKUP_DIRNAME} and ${LMOUNTPOINT}/${LRESTORE_DIRNAME}." ozo-log
+              RETURN=1
+            fi
+          else
+            # mount failed; log
+            LEVEL="err" MESSAGE="Unable to mount ${LUUID} to ${LMOUNTPOINT}." ozo-log
+            RETURN=1
+          fi
+        else
+          # mountpoint doesn't exist; log
+          LEVEL="err" MESSAGE="Mountpoint ${LMOUNTPOINT} does not exist." ozo-log
+          RETURN=1
+        fi
+      fi
+    else
+      # missing LUUID
+      LEVEL="err" MESSAGE="Could not find ${LUUID} on this system." ozo-log
+      RETURN=1
+    fi
+  else
+    # missing mount command
+    LEVEL="err" MESSAGE="Missing 'mount' command" ozo-log
+    RETURN=1
+  fi
+  return ${RETURN}
+}
 
-# Exclude the rdiff volume mount point
-DEF_EXCLUDES="${DEF_EXCLUDES},${MOUNTPOINT}"
+function ozo-umount-uuid {
+  ### Unmounts UUID from LMOUNTPOINT
+  ### Returns 0 (TRUE) if the umount is sucessful and 1 (FALSE) if it fails
+  local RETURN=0
+  # check if the UUID is mounted to the volume
+  if mount | grep $(readlink -f /dev/disk/by-uuid/${LUUID}) | grep ${LMOUNTPOINT} > /dev/null
+  then
+    # LUUID is mounted to LMOUNTPOINT; attempt to unmount
+    if ! umount ${LMOUNTPOINT}
+    then
+      # umount failed; log
+      LEVEL="err" MESSAGE="Could not unmount ${LMOUNTPOINT} (is it busy?)" ozo-log
+      RETURN=1
+    fi
+  else
+    # volume is already unmounted; log
+    LEVEL="warning" MESSAGE="Volume ${LUUID} was not found mounted to ${LMOUNTPOINT}." ozo-log
+  fi
+  return ${RETURN}
+}
 
-# Check that a configuration file was specified
-if [ -n "${1}" ]
+function ozo-fsck-uuid {
+  ### Performs an fsck of the Rdiff-Backup volume
+  ### Returns 0 (TRUE) if successful and 1 (FALSE) if not
+  local RETURN=0
+  if ozo-umount-uuid
+  then
+    if ! /sbin/fsck UUID=${LUUID}
+    then
+      RETURN=1
+    fi
+  else
+    LEVEL="err" MESSAGE="Volume ${LUUID} cannot be unmounted; unable to fsck." ozo-log
+  fi
+  return ${RETURN}
+}
+
+function ozo-validate-configuration {
+  ### Performs a series of checks against the script configuration
+  ### Returns 0 (TRUE) if all checks pass and 1 (FALSE) if any check fails
+  local RETURN=0
+  # check that all user-defined variables are set
+  for USERDEFVAR in LCONF_DIR LUUID LMOUNTPOINT LBACKUP_DIRNAME LRESTORE_DIRNAME RDEF_INCLUDES RDEF_EXCLUDES LFSCK_DAY
+  do
+    if [[ -z "${!USERDEFVAR}" ]]
+    then
+      RETURN=1
+      LEVEL="err" MESSAGE="User-defined variable ${USERDEFVAR} is not set." ozo-log
+    fi
+  done
+  # check that at least one job configuration file has been specified
+  if [[ $(ls ${LCONF_DIR}) < 1 ]]
+  then
+    LEVEL="err" MESSAGE="No job configuration files found in ${LCONF_DIR}." ozo-log
+    RETURN=1
+  fi
+  # check that the device is present and mountable
+  if ozo-mount-uuid
+  then
+    # device is present and mounting was successful; unmount
+    if ! ozo-umount-uuid
+    then
+      # device could not be unmounted
+      RETURN=1
+  else
+    RETURN=1
+  fi
+  # check that the ssh binary exists
+  if which ssh
+  then
+    # check that SSH with keys is possible
+    if ssh -p ${RSSHPORT} -o BatchMode=yes ${RHOSTUSER}@${RHOSTFQDN} true
+    then
+      # check that the remote system has zfs
+      if ! ssh -p ${RSSHPORT} ${RUSER}@${RHOSTFQDN} which rdiff-backup
+      then
+        LEVEL="err" MESSAGE="Remote host ${RHOSTFQDN} is missing Rdiff-backup." ozo-log
+        RETURN=1
+      fi
+    else
+      RETURN=1
+      LEVEL="err" MESSAGE="Unable to SSH to ${RHOSTFQDN} with keys." ozo-log
+    fi  
+  else
+    LEVEL="err" MESSAGE="Local system is missing SSH." ozo-log
+    RETURN=1
+  fi
+  return ${RETURN}
+}
+
+function ozo-validate-job {
+  ### Performs a series of checks against the job configuration
+  ### Returns 0 (TRUE) if all checks pass and 1 (FALSE) if any check fails
+  local RETURN=0
+  # check that all user-defined variables are set
+  for USERDEFVAR in RHOSTUSER RHOSTFQDN RAGE
+  do
+    if [[ -z "${!USERDEFVAR}" ]]
+    then
+      LEVEL="err" MESSAGE="User-defined variable ${USERDEFVAR} is not set." ozo-log
+      RETURN=1
+    fi
+  done
+  # if RSSHPORT is omitted, set it to 22 and hope for the best
+  if [[ -z "${RSSHPORT}" ]]
+  then
+    RSSHPORT=22
+  fi
+  # job specific (derived) variables
+  # create the "inclusions" string from $RDEF_INCLUDES and (if set) $RHOST_INCLUDES
+  INCLUDES="--include ${RDEF_INCLUDES//,/ --include }"
+  if [ -n "${RHOST_INCLUDES}" ]
+  then
+    INCLUDES="${INCLUDES} --include ${RHOST_INCLUDES//,/ --include }"
+  fi
+  # create the "exclusions" string from $RDEF_EXCLUDES and (if set) $RHOST_EXCLUDES
+  EXCLUDES="--exclude ${RDEF_EXCLUDES//,/ --exclude }"
+  if [ -n "${RHOST_EXCLUDES}" ]
+  then
+    EXCLUDES="${EXCLUDES} --exclude ${RHOST_EXCLUDES//,/ --exclude }"
+  fi
+  # exclude the rdiff volume mount point (this handles the case where the Remote System is the Rdiff-Backup System)
+  EXCLUDES="${EXCLUDES},${LMOUNTPOINT}"
+  # directory for storing increments for this job
+  RHOSTFQDN_INCREMENTS_DIR="${LMOUNTPOINT}/${LBACKUP_DIRNAME}/${RHOSTFQDN}"
+  # attempt to create job increments directory
+  if ! mkdir -p ${RHOSTFQDN_INCREMENTS_DIR}
+  then
+    LEVEL="err" MESSAGE="Unable to create ${RHOSTFQDN_INCREMENTS_DIR} to store increments for ${RHOSTFQDN}." ozo-log
+    RETURN=1
+  fi
+}
+
+function ozo-rdiff-backup {
+  ### Performs the configured rdiff-backup for a remote host
+  ### Returns 0 (TRUE) if the job is successful and 1 (FALSE) if there are any errors
+  local RETURN=0
+  LEVEL="info" MESSAGE="Starting Rdiff-Backup job." ozo-log
+  if $(/usr/bin/rdiff-backup --verbosity 0 --create-full-path --remote-schema "/usr/bin/ssh -p ${SSHPORT} -C  %s /usr/bin/rdiff-backup --server --restrict-read-only /" ${INCLUDES} ${EXCLUDES} root@${HOST}::/ ${RHOSTFQDN_INCREMENTS_DIR})
+  then
+    # rdiff-backup succeeded; log
+    LEVEL="info" MESSAGE="Rdiff-Backup job finished with success." ozo-log
+  else
+    # rdiff-backup failed; log
+    LEVEL="err" MESSAGE="Rdiff-Backup job failed." ozo-log
+    RETURN=1
+  fi   
+}
+
+function ozo-rdiff-maintenance {
+  ### Performs maintenance on an rdiff-backup increment set
+  ### Returns 0 (TRUE) if maintenance is successful and 1 (FALSE) if it fails
+  local RETURN=0
+  # attempt to remove old increments
+  LEVEL="info" MESSAGE="Performing maintenance on increments for ${RHOSTFQDN}." ozo-log
+  if /usr/bin/rdiff-backup --force --remove-older-than ${RAGE}D ${RHOSTFQDN_INCREMENTS_DIR}
+  then
+    LEVEL="info" MESSAGE="Successfully removed increments older than ${RAGE} days for ${RHOSTFQDN}." ozo-log
+  else
+    LEVEL="err" MESSAGE="Unable to remove increments older than ${RAGE} days for ${RHOSTFQDN}." ozo-log
+    RETURN=1
+  fi
+  return ${RETURN}
+}
+
+function ozo-program-loop {
+  ### Validates the script configuration
+  ### Validates and runs jobs
+  ### Performs increments maintenance
+  ### Performs filesystem maintenance
+  ### Returns 0 (TRUE) if the configuration validates and all jobs run and 1 (FALSE) if the configuration does not validate or any job fails
+  local RETURN=0
+  local CONFIGURATION="/etc/rdiff-backup.conf"
+  # check if the configuration file exists and is readable
+  if [[ -f "${CONFIGURATION}" ]]
+  then
+    # file is readable; source
+    source "${CONFIGURATION}"
+    # validate the configuration
+    if ozo-validate-configuration
+    then
+      # configuration validates; mount UUID
+      if ozo-mount-uuid
+      then
+        # UUID mounted, iterate through the jobs
+        for JOB in $(ls "${LCONF_DIR}/*conf")
+        do
+          # validate the job
+          if ozo-validate-job
+          then
+            # job validates; perform backup
+            if ozo-rdiff-backup
+            then
+              # backup succeeded; perform increments maintenance
+              if ! ozo-rdiff-maintenance
+              then
+                # increments maintenance failed
+                RETURN=1
+              fi
+            else
+              # backup failed
+              RETURN=1
+            fi
+          else
+            # job did not validate
+            RETURN=1
+          fi
+        done
+        # iteration complete; attempt to umount UUID
+        if ozo-umount-uuid
+        then
+          # UUID unmounted; check if it's fsck day
+          if [[ "$( date +%u )" == "${LFSCK_DAY}" ]]
+          then
+            # it's fsck day!; attempt to fsck
+            if ! ozo-fsck-uuid
+            then
+              # fsck failed
+              RETURN=1
+            fi
+          fi
+        else
+          # umount UUID failed
+          RETURN=1
+        fi
+      else
+        # mount UUID failed
+        RETURN=1
+      fi
+    else
+      # configuration validation failed
+      RETURN=1
+    fi    
+  else
+    # configuraiton file does not exist or is not readable
+    LEVEL="err" MESSAGE="Configuration file ${CONFIGURATION} does not exist or is not readable." ozo-log
+  fi
+  return ${RETURN}
+}
+
+# MAIN
+LEVEL="info" MESSAGE="Rdiff-Backup starting." ozo-log
+if ozo-program-loop
 then
-	# Configuration file exists; source it
-	. ${1}
-	# Check that the configuration file has HOST and AGE set
-	if [ "x${HOST}" == "x" -o "x${AGE}" == "x" ]
-	then
-		echo ""
-		echo "Configuration file ${1} does not contain values for HOST and/or AGE."
-		echo ""
-		exit 1
-	#else
-		# Configuration file has values for HOST and AGE
-	fi
+  # run was successful
+  LEVEL="info" MESSAGE="Rdiff-Backup finished with success." ozo-log
 else
-	# Configuration file was not specified
-	echo "A configuration file was not specified. Usage:"
-	echo ""
-	echo "${0} ${CONF_DIR}/[HOSTNAME].conf"
-	echo ""
-	exit 1
-fi
-
-# check if our device is present in the system
-if ls /dev/disk/by-uuid/${UUID} > /dev/null
-then
-	# device is present.
-	# check if our device is already mounted 
-	if ! /bin/mount | grep ${MOUNTPOINT} > /dev/null
-	then
-		# our device is not mounted
-		# try to mount it
-		if ! /bin/mount UUID=${UUID} ${MOUNTPOINT}
-		then
-			# our device did not mount 
-			echo "failed to mount the destination device"
-			exit 1
-		#else
-			# our device mounted properly
-		fi
-	#else
-		# our device is already mounted
-	fi
-	# define and create the destination directory
-	DESTINATION="${MOUNTPOINT}/backup"
-	mkdir -p ${DESTINATION}
-else
-	# device is not present
-	echo "destination device is not present"
-	exit 1
-fi
-
-# create the "inclusions" string from $DEF_INCLUDES and $HOST_INCLUDES (if set)
-INCLUDES="--include ${DEF_INCLUDES//,/ --include }"
-if [ -n "${HOST_INCLUDES}" ]
-then
-	INCLUDES="${INCLUDES} --include ${HOST_INCLUDES//,/ --include }"
-fi
-
-# create the "exclusions" string from $DEF_EXCLUDES and $HOST_EXCLUDES (if set)
-EXCLUDES="--exclude ${DEF_EXCLUDES//,/ --exclude }"
-if [ -n "${HOST_EXCLUDES}" ]
-then
-	EXCLUDES="${EXCLUDES} --exclude ${HOST_EXCLUDES//,/ --exclude }"
-fi
-
-# perform the rdiff-backup
-INCREMENTS_DIR="${DESTINATION}/${HOST}"
-mkdir -p ${INCREMENTS_DIR}
-if $( /usr/bin/rdiff-backup --verbosity 0 --create-full-path --remote-schema "/usr/bin/ssh -p ${SSHPORT} -C  %s /usr/bin/rdiff-backup --server --restrict-read-only /" ${INCLUDES} ${EXCLUDES} root@${HOST}::/ ${INCREMENTS_DIR} )
-then
-	# rdiff-backup was successful
-	# remove old increments
-	if ! /usr/bin/rdiff-backup --force --remove-older-than ${AGE}D ${INCREMENTS_DIR}
-	then
-		echo "unable to remove old increments"
-	#else
-		# the increments were removed successfully
-	fi
-else
-	# rdiff-backup was not successful
-	echo "rdiff did not complete successfully"
-fi
-
-# unmount the destination
-if ! umount ${MOUNTPOINT}
-then
-	# destination did not unmount
-	echo "failed to unmount destination device. is it busy?"
-	exit 1
-else
-	# destination unmounted
-	# if Monday (1), run an fsck
-	if [ $( date +%u ) == ${FSCK_DAY} ]
-	then
-		/sbin/fsck UUID=${UUID}
-	fi
+  # run failed one or more jobs
+  LEVEL="err" MESSAGE="Rdiff-Backup finished with errors." ozo-log
 fi
